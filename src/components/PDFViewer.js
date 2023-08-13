642ec9776f39c82c6e4a6dbe9ee193d730cafc9f
Arbab Ali <arbab.ali@hubble42.com>
2023-08-13 08:50:29 -0700

    Jumo To Page Implemented

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,88 +1,8 @@
-// import React, { useState, useEffect } from 'react'
-// import './PDFViewer.css'
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-// import { Viewer, Worker } from '@react-pdf-viewer/core'
-// import '@react-pdf-viewer/core/lib/styles/index.css'
-// import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
-// const PDFViewer = () => {
-//     const [pdfFile, setPdfFile] = useState(null)
-//     const [viewPdf, setViewPdf] = useState(null)
-//     const [searchText, setSearchText] = useState('')
-//     const [searchPlugin, setSearchPlugin] = useState(null)
+import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
 
-//     const fileType = ['application/pdf']
-
-//     const handleChange = (e) => {
-//         let selectedFile = e.target.files[0]
-//         if (selectedFile) {
-//             if (selectedFile && fileType.includes(selectedFile.type)) {
-//                 let reader = new FileReader()
-//                 reader.readAsDataURL(selectedFile)
-//                 reader.onload = (e) => {
-//                     setPdfFile(e.target.result)
-//                 }
-//             } else {
-//                 setPdfFile(null)
-//             }
-//         } else {
-//             console.log('No file Selected')
-//         }
-//     }
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//         if (pdfFile !== null) {
-//             setViewPdf(pdfFile)
-//         } else {
-//             setViewPdf(null)
-//         }
-//     }
-
-//     // const handleSearch = () => {
-//     //     searchPlugin.instance.search(searchText)
-//     // }
-
-//     // const handleSearchTextChange = (e) => {
-//     //     setSearchText(e.target.value)
-//     // }
-
-//     // useEffect(() => {
-//     //     setSearchPlugin(defaultLayoutPlugin)
-//     // }, [])
-
-//     return (
-//         <div className='container'>
-//             <form onSubmit={handleSubmit}>
-//                 <input type='file' className='form-control' onChange={handleChange} />
-//                 <button type='submit' className='btn btn-success'>
-//                     View pdf
-//                 </button>
-//             </form>
-
-//             <h2>View pdf</h2>
-//             <div className='pdf-container'>
-//                 <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-//                     {viewPdf && searchPlugin && (
-//                         <>
-//                             {/* <div className='search-container'>
-//                                 <input type='text' value={searchText} onChange={handleSearchTextChange} />
-//                                 <button onClick={handleSearch}>Search</button>
-//                             </div> */}
-//                             <Viewer fileUrl={viewPdf} plugins={[searchPlugin]} />
-//                         </>
-//                     )}
-//                     {/* {viewPdf && !searchPlugin && <div>Loading...</div>} */}
-//                     {!viewPdf && <div>No Pdf</div>}
-//                 </Worker>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default PDFViewer
-
-import React, { useState } from 'react'
+import '@react-pdf-viewer/page-navigation/lib/styles/index.css';
+import React, { useRef, useState } from 'react'
 import './PDFViewer.css'
 import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
@@ -93,7 +13,9 @@ const PDFViewer = () => {
     const [viewPdf, setViewPdf] = useState(null)
 
     const fileType = ['application/pdf']
-
+    const pageNavigationPluginInstance = pageNavigationPlugin();
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const { jumpToPage } = pageNavigationPluginInstance;
     const handleChange = (e) => {
         let selectedFile = e.target.files[0]
         if (selectedFile) {
@@ -122,9 +44,10 @@ const PDFViewer = () => {
             setViewPdf(null)
         }
     }
+    // Ref to access the viewer's API functions
+    const viewerRef = useRef();
 
-    const newpluggin = defaultLayoutPlugin()
-
+ 
   return (
     <div className='container'>
         <form onSubmit={handleSubmit}>
@@ -132,13 +55,25 @@ const PDFViewer = () => {
             <button type='submit' className='btn btn-success'  >
                 View pdf
             </button>
+            <br />
+            <button onClick={() => {
+                jumpToPage(30)
+                console.log(viewerRef);
+
+            }}>
+                Click me
+            </button>
         </form>
 
-        <h2>View pdf</h2>
+        <h2 >View pdf</h2>
         <div className='pdf-container'>
-            <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+            <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js' > 
                 {viewPdf && <>
-                        <Viewer fileUrl={viewPdf} plugins={[newpluggin]} />
+                        <Viewer 
+                        fileUrl={viewPdf}
+                         // Correct placement of the ref
+                        plugins={[pageNavigationPluginInstance]}
+                        />
                 </>}
                 {!viewPdf && <>No Pdf</>}
             </Worker>
